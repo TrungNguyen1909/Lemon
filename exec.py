ec9ba6f40a569fad732da82092daba0e622911ca
Nguyễn Hoàng Trung <ntrung03@outlook.com>
2019-05-01 21:18:14 +0700

    Oops

--- exec.py
+++ exec.py
@@ -3,7 +3,7 @@ if 'PORT' in os.environ:
 	fd =  open("/app/.heroku/python/lib/python3.7/site-packages/discord/gateway.py","r")
 	lines = fd.readlines()
 	fd.close()
-	lines[686] = "            interval = (data['heartbeat_interval'] / 1000.0)*0.75"
+	lines[686] = "            interval = (data['heartbeat_interval'] / 1000.0)*0.75\n"
 	fd = open("/app/.heroku/python/lib/python3.7/site-packages/discord/gateway.py","w")
 	for line in lines:
 		fd.write(line)
