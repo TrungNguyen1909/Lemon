06df7598d3f51f51a30f457878d172b69f5f66ca
Nguyễn Hoàng Trung <ntrung03@outlook.com>
2019-05-01 21:14:20 +0700

    Oops

---
+++ exec.py
@@ -0,0 +1,13 @@
+import os
+if 'PORT' in os.environ:
+	fd =  open("/app/.heroku/python/lib/python3.7/site-packages/discord/gateway.py","r")
+	lines = fd.readlines()
+	fd.close()
+	lines[686] = "            interval = (data['heartbeat_interval'] / 1000.0)*0.75"
+	fd = open("/app/.heroku/python/lib/python3.7/site-packages/discord/gateway.py","w")
+	for line in lines:
+		fd.write(line)
+	fd.close()
+
+import bot
+
