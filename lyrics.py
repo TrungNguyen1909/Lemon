0e8b8bf9b298a9de939eb9fc42751dedfc302ec0
Trung Nguyen <ntrung03@outlook.com>
2019-07-21 19:30:20 +0700

    Update lyrics translation

--- lyrics.py
+++ lyrics.py
@@ -4,25 +4,6 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 USER_TOKEN = os.getenv('MXM_TOKEN')
-def getlyricsTranslation(track_id):
-	params = (
-			('commontrack_id',str(track_id)),
-			('usertoken', USER_TOKEN),	
-			('selected_language','en'),
-			('format', 'json'),
-			('user_language', 'en'),
-			('tags', 'nowplaying'),
-			('namespace', 'track_translation'),
-			('f_subtitle_length_max_deviation', '1'),
-			('subtitle_format', 'mxm'),
-			('app_id', 'web-desktop-app-v1.0'),
-			('translation_fields_set','minimal'),
-			
-		)
-	apiurl = 'https://apic.musixmatch.com/ws/1.1/crowd.track.translations.get'
-	response = requests.get(apiurl,params = params)
-	d = json.loads(response.text)
-	return d['message']['body']['translations_list']
 def getlyrics(track,album,artist,duration = None):
 	params = [
 		('format', 'json'),
@@ -30,11 +11,13 @@ def getlyrics(track,album,artist,duration = None):
 		('q_artist', artist),
 		('q_album', album),
 		('user_language', 'en'),
-		('tags', 'nowplaying'),
+		('tags', 'playing'),
 		('namespace', 'lyrics_synched'),
 		('f_subtitle_length_max_deviation', '1'),
 		('subtitle_format', 'mxm'),
-		('app_id', 'web-desktop-app-v1.0'),
+		('app_id', 'mac-ios-v2.0'),
+		('part','subtitle_translated,lyrics_translated'),
+		('selected_language','en'),
 		('usertoken', USER_TOKEN),	
 	]
 	if duration:
@@ -43,11 +26,9 @@ def getlyrics(track,album,artist,duration = None):
 	result = {}
 	result['error']=False
 	try:
-		response = requests.get('https://apic-desktop.musixmatch.com/ws/1.1/macro.subtitles.get', params=params, verify=False)
+		response = requests.get('https://apic.musixmatch.com/ws/1.1/macro.subtitles.get', params=params, verify=False)
 		d = json.loads(response.text)
 		lyrics = d['message']['body']['macro_calls']['track.lyrics.get']['message']['body']['lyrics']['lyrics_body']
-		#print(lyrics)
-		trackid = d['message']['body']['macro_calls']['matcher.track.get']['message']['body']['track']['commontrack_id']
 		result['txt'] = lyrics
 		result['has_lrc'] = False
 		try:
@@ -62,23 +43,21 @@ def getlyrics(track,album,artist,duration = None):
 			if result['lang'] == 'en':
 				return result
 			try:
-				trans = getlyricsTranslation(trackid)
-				trans_map = {}
-				for tran in trans:
-					tran = tran['translation']
-					trans_map[tran['subtitle_matched_line']] = tran['description']
+				subtitle_translated = json.loads(d['message']['body']['macro_calls']['track.subtitles.get']['message']['body']['subtitle_list'][0]['subtitle']['subtitle_translated']['subtitle_body'])
 				translated = []
-				for line in result['lrc']:
+				for line in subtitle_translated:
 					try:
-						line['en_text'] = trans_map[line['text']]
+						line['en_text'] = line['text']				
 					except:
 						line['en_text'] = ''
 					finally:
+						line['text'] = line['original']	
 						translated.append(line)
 				result['lrc'] = translated
 				result['has_trans'] = True
 				return result
-			except:
+			except Exception as e:
+				print(e)
 				return result
 			finally:
 				return result
@@ -87,6 +66,6 @@ def getlyrics(track,album,artist,duration = None):
 		return result
 		
 if __name__ == "__main__":
-	getlyrics("The best thing i ever did", "The year of YES", artist = "TWICE")
-	getlyricsTranslation(90555411)
+	result = getlyrics("The best thing i ever did", "The year of YES", artist = "TWICE")
+	print(result)
 
