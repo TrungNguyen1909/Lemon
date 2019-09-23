8c1c4090d8bffbd79a00654a602b5fddb78bf65f
Trung Nguyễn <trung@Trungs-MBP.lan>
2019-09-23 16:10:28 +0700

    Fix code patcher

--- exec.py
+++ exec.py
@@ -1,20 +1,11 @@
 import os
 if 'PORT' in os.environ:
-	fd =  open("/app/.heroku/python/lib/python3.7/site-packages/discord/gateway.py","r")
-	lines = fd.readlines()
-	fd.close()
-	lines[686] = "            interval = (data['heartbeat_interval'] / 1000.0)*0.75\n"
-	fd = open("/app/.heroku/python/lib/python3.7/site-packages/discord/gateway.py","w")
-	for line in lines:
-		fd.write(line)
-	fd.close()
 	fd = open("/app/.heroku/python/lib/python3.7/site-packages/discord/opus.py","r")
-	lines = fd.readlines()
+	content = fd.read()
 	fd.close()
-	lines[109] = "        _lib = libopus_loader('libopus.so.0')\n"
+	content = content.replace("_lib = libopus_loader(ctypes.util.find_library('opus'))","_lib = libopus_loader('libopus.so.0')")
 	fd = open("/app/.heroku/python/lib/python3.7/site-packages/discord/opus.py","w")
-	for line in lines:
-		fd.write(line)
+	fd.write(content)
 	fd.close()
 import bot
 
