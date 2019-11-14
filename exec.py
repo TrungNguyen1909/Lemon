6917bfd180afd6e870374e9698dacc4b463c648e
Trung Nguyen <ntrung03@outlook.com>
2019-11-14 22:30:37 +0700

    revert f520954, update dependency, runtime

--- exec.py
+++ exec.py
@@ -7,5 +7,12 @@ if 'PORT' in os.environ:
 	fd = open("/app/.heroku/python/lib/python3.7/site-packages/discord/opus.py","w")
 	fd.write(content)
 	fd.close()
+	fd = open("/app/.heroku/python/lib/python3.7/site-packages/discord/gateway.py","r")
+	content = fd.read()
+	fd.close()
+	content = content.replace("interval = data['heartbeat_interval'] / 1000.0","interval = data['heartbeat_interval'] / 1000.0 * 0.75")
+	fd = open("/app/.heroku/python/lib/python3.7/site-packages/discord/gateway.py","w")
+	fd.write(content)
+	fd.close()
 import bot
 
